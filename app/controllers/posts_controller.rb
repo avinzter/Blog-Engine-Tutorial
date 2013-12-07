d434eef1d6ab305f344129562210aa507ea8c8eb
Geniva B. Lacara <geniva.lacara@gmail.com>
2013-12-07 15:43:26 -0600

    Modify index view.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,8 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
 
+    @post = Post.order("created+at DESC")
+
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @posts }
